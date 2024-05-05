8e5b1f8788df764f8dd322abaaaa4c5f9022b0ac
RyanRonish <roni0210@bears.unco.edu>
2024-05-05 13:19:34 -0600

    html templates

--- se/models.py
+++ se/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
--- se/views.py
+++ se/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Service, ContactMessage
 
-# Create your views here.
+def index(request):
+    return render(request, 'landscaping/index.html')
+
+def services(request):
+    services = Service.objects.all()
+    return render(request, 'landscaping/services.html', {'services': services})
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        ContactMessage.objects.create(name=name, email=email, message=message)
+        return render(request, 'landscaping/contact_success.html')
+    return render(request, 'landscaping/contact.html')
